4a7b9b63e3c85aa892c0edf6779352430398c4e0
anupam verma <92857132+anupam2747@users.noreply.github.com>
2022-03-01 17:10:07 +0530

    cc

--- src/route.php
+++ src/route.php
@@ -1,11 +1,12 @@
 <?php
-
+                                    
 Route::get('calculator',function(){
     echo "helllo";
-});
+});  
+                                         
+Route::get('add/{a}/{b}',[LP\Calculator\CalculatorController::class,'add']);            
+Route::get('substract/{a}/{b}',[LP\Calculator\CalculatorController::class,'substract']);         
+                                                                                  
+?>       
 
-
-Route::get('add/{a}/{b}',[LP\Calculator\CalculatorController::class,'add']);
-Route::get('substract/{a}/{b}',[LP\Calculator\CalculatorController::class,'substract']);
-
-?>
+    
